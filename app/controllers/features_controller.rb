b1f040c3687940392d3d4c827e6f2170b273c1d9
Sean Doyle <sean.p.doyle24@gmail.com>
2013-11-25 00:42:33 -0500

    redirect to posts when nothing is featured on the splash

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,4 +1,12 @@
 class FeaturesController < InheritedResources::Base
+  def index
+    if featured.present?
+      super
+    else
+      redirect_to posts_path
+    end
+  end
+
   def featured
     @featured ||= Feature.featured
   end
--- spec/features/root_page_spec.rb
+++ spec/features/root_page_spec.rb
@@ -1,20 +1,30 @@
 require 'spec_helper'
 
 feature 'Splash page' do
-  given(:feature) { build :featured_feature }
+  context 'when nothing is featured' do
+    scenario 'the user is redirected to the posts page' do
+      visit root_path
 
-  background do
-    feature.save!
-    visit root_path
+      expect(current_path).to eq posts_path
+    end
   end
 
-  scenario 'displays the newest feature video' do
-    expect(page).to have_feature feature
-  end
+  context 'when a feature exists' do
+    given(:feature) { build :featured_feature }
 
-  scenario 'has a link to enter the site' do
-    find('#enter-site').click
+    background do
+      feature.save!
+      visit root_path
+    end
 
-    expect(current_path).to eq posts_path
+    scenario 'displays the newest feature video' do
+      expect(page).to have_feature feature
+    end
+
+    scenario 'has a link to enter the site' do
+      find('#enter-site').click
+
+      expect(current_path).to eq posts_path
+    end
   end
 end
