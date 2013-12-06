a6e1e006cedb7de091810db35a29a265cba37bb3
Sean Doyle <sean.p.doyle24@gmail.com>
2013-12-06 11:50:45 -0500

    added lightbox to visuals

---
+++ app/helpers/lightbox_helper.rb
@@ -0,0 +1,11 @@
+module LightboxHelper
+  def lightbox_tag(thumbnail, url, caption = nil, group = nil, **opts)
+    link_opts         = {}
+    link_opts[:rel]   ||= "lightbox[#{group}]" if group.present?
+    link_opts[:title] ||= caption if caption.present?
+
+    link_to url, link_opts do
+      image_tag thumbnail, opts
+    end
+  end
+end
