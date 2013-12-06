38e2283463829e929bb676b8733bce8b9d148c10
Sean Doyle <sean.p.doyle24@gmail.com>
2013-12-06 12:32:46 -0500

    Fixed inconsistent paging
    * Added a `max_per_page` to Kaminari config
    * Added a `has_scope :page, default: 1` to `ApplicationController`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  has_scope :page, default: 1
+
   def featured_project
     @featured_project ||= Project.featured
   end
