47305a9ca2c7e857a7eefba7e072abab0105c810
Sean Doyle <sean.p.doyle24@gmail.com>
2013-12-07 17:49:35 -0500

    added memcachier in production and staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,8 @@ require Rails.root.join('config/initializers/smtp')
 CasteServer::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.cache_store = :dalli_store
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
