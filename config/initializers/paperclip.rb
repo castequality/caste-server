8eb0018bc4d82e749a73234f04231ce3214f1c2d
Sean Doyle <sean.p.doyle24@gmail.com>
2013-12-07 19:43:32 -0500

    updated paperclip settings

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,10 +1,11 @@
 storage = (Rails.env.staging? || Rails.env.production?) ? :s3 : :file
 
-Rails.configuration.paperclip_defaults = {
+Paperclip::Attachment.default_options.merge!(
   storage: storage,
+  bucket: ENV['S3_BUCKET_NAME'],
+  url: "/system/:class/:attachment/:id/:style/:filename",
   s3_credentials: {
-    bucket: ENV['S3_BUCKET_NAME'],
-    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    access_key_id: ENV['S3_ACCESS_KEY_ID'],
+    secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
   }
-}
+)
