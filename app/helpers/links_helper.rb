17daee8a04d5ac00b86af39762545c58f45fe11a
Sean Doyle <sean.p.doyle24@gmail.com>
2013-12-08 01:59:37 -0500

    Added bigcartel support

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -3,12 +3,23 @@ module LinksHelper
     "#{name}-link"
   end
 
+  def store_link_to(**opts)
+    opts[:class]  ||= "store-link"
+    opts[:target] ||= "_blank"
+
+    big_cartel = ReadsBigCartel.new
+
+    product = big_cartel.random_product
+
+    render "store_link", url: product.url, thumbnail: product.thumbnail
+  end
+
   def instagram_link_to(**opts)
     opts[:class]  ||= "instagram-link"
     opts[:target] ||= "_blank"
 
     instagram = ReadsInstagram.new
 
-    render "instagram_link", url: instagram.url, newest: instagram.newest
+    render "instagram_link", url: instagram.url, thumbnail: instagram.newest
   end
 end
