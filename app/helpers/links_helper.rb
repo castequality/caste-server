d10539934d87014aeb93f68f56f78418dcd20bd2
Sean Doyle <sean.p.doyle24@gmail.com>
2014-09-05 09:16:28 -0400

    Adds imagery

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -10,11 +10,4 @@ module LinksHelper
   def paging_links(**opts)
     render 'paging', opts
   end
-
-  def header_link_to(route, link_path = nil)
-    link_text = t("header.links.#{route}")
-    link_path ||= __send__("#{route}_path")
-
-    link_to link_text, link_path, id: "#{route}-link"
-  end
 end
