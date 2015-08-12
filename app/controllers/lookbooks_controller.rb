5f089e740e6312048e5a09a7f4a06efbc47e4e1a
Sean Doyle <sean.p.doyle24@gmail.com>
2015-08-11 23:37:52 -0400

    Add Traffic Lookbook

--- app/controllers/lookbooks_controller.rb
+++ app/controllers/lookbooks_controller.rb
@@ -9,11 +9,12 @@ class LookbooksController < StaticController
     [{
       id: "fall2014",
       pages: [
-        { url: image_url("lookbooks/fall2014/1.jpg") },
-        { url: image_url("lookbooks/fall2014/2.jpg") },
-        { url: image_url("lookbooks/fall2014/3.jpg") },
-        { url: image_url("lookbooks/fall2014/4.jpg") },
-        { url: image_url("lookbooks/fall2014/5.jpg") },
+        { url: image_url("lookbooks/traffic/1.jpg") },
+        { url: image_url("lookbooks/traffic/2.jpg") },
+        { url: image_url("lookbooks/traffic/3.jpg") },
+        { url: image_url("lookbooks/traffic/4.jpg") },
+        { url: image_url("lookbooks/traffic/5.jpg") },
+        { url: image_url("lookbooks/traffic/6.jpg") },
       ]
     }]
   end
