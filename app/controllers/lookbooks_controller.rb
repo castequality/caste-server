b59ec567a06519a13ce31f346eeb312eff99837e
Sean Doyle <sean.p.doyle24@gmail.com>
2016-02-23 23:53:53 -0500

    Add AIME Lookbook

--- app/controllers/lookbooks_controller.rb
+++ app/controllers/lookbooks_controller.rb
@@ -7,14 +7,9 @@ class LookbooksController < StaticController
 
   def build_lookbooks
     [{
-      id: "indigo",
+      id: "aime-de-magenta",
       pages: [
-        { url: vimeo_url(138048069), video: true },
-        { url: image_url("lookbooks/indigo/1.jpg") },
-        { url: image_url("lookbooks/indigo/2.jpg") },
-        { url: image_url("lookbooks/indigo/3.jpg") },
-        { url: image_url("lookbooks/indigo/4.jpg") },
-        { url: image_url("lookbooks/indigo/5.jpg") },
+        { url: vimeo_url(155997209), video: true },
       ]
     }]
   end
